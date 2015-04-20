53073f5169f720f2646d709166d86f696261cdc0
Evan Butler <evanb2@mac.com>
2015-04-20 11:20:55 -0700

    add form in html, with edit.name and delete.restaurant functionality

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,20 +1,22 @@
 bestRestaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
   $scope.restaurants = [];
   $scope.addRestaurant = function() {
-    $scope.restaurants.push({ name: $scope.restaurantName });
+    $scope.restaurants.push({ name: $scope.restaurantName,
+                              phone: $scope.restaurantPhone,
+                              street: $scope.restaurantStreet,
+                              city: $scope.restaurantCity,
+                              state: $scope.restaurantState,
+                              zip: $scope.restaurantZip,
+                              website: $scope.restaurantWebsite,
+                              description: $scope.restaurantDescription });
     $scope.restaurantName = null;
-    $scope.restaurants.push({ phone: $scope.restaurantPhone });
     $scope.restaurantPhone = null;
-    $scope.restaurants.push({ street: $scope.restaurantStreet });
     $scope.restaurantStreet = null;
-    $scope.restaurants.push({ city: $scope.restaurantCity });
     $scope.restaurantCity = null;
-    $scope.restaurants.push({ state: $scope.restaurantState });
     $scope.restaurantState = null;
-    $scope.restaurants.push({ zip: $scope.restaurantZip });
     $scope.restaurantZip = null;
-    $scope.restaurants.push({ website: $scope.restaurantWebsite });
     $scope.restaurantWebsite = null;
+    $scope.restaurantDescription = null;
   };
   $scope.deleteRestaurant = function(restaurant) {
     var index = $scope.restaurants.indexOf(restaurant);
